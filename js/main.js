6d3de1e51c177e41419cef0b4dcc4a332946e837
Anton <anton.mogilev@mail.ru>
2021-10-07 13:32:40 +0300

    Больше деталей. Исправил ощибки

--- js/main.js
+++ js/main.js
@@ -3,31 +3,32 @@ const LAT_MAX = 35.70000;
 const LNG_MIN = 139.70000;
 const LNG_MAX = 139.80000;
 const COMMA_LAT_LNG = 5;
-const CHECKIN_AND_CHECKOUT = [
+const NUMBER_ANNOUNCEMENT = 10;
+const CHECKIN_AND_CHECKOUT_TIMES = [
   '12:00',
   '13:00',
   '14:00',
 ];
-const TYPE_BUILDING = [
+const BUILDING_TYPES = [
   'palace',
   'flat',
   'house',
   'bungalow',
   'hotel',
 ];
-const DESCRIPTION_OFFER = [
+const OFFER_DESCRIPTIONS = [
   'Закрытая парковка',
   'Панорамное остекление',
   'Круглосуточный доступ',
 ];
-const TITLE_OFFER = [
+const OFFER_TITLES = [
   'Заголовок 1',
   'Заголовок 2',
   'Заголовок 3',
   'Заголовок 4',
   'Заголовок 5',
 ];
-const FEATURES_OFFER = [
+const OFFER_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -35,7 +36,7 @@ const FEATURES_OFFER = [
   'elevator',
   'conditioner',
 ];
-const PHOTOS_OFFER = [
+const OFFER_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
@@ -64,41 +65,41 @@ function getRandomNumberComma(min, max, comma) {
 }
 getRandomNumberComma();
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomNumber(0, elements.length - 1)];
-};
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 const getArrayElements = (array) => {
   const randomLength = getRandomNumber(1, array.length-1);
-  const randomArray = Array.from({length:randomLength});
-  let cloneArray = array.slice();
+  const randomArray = [];
+  const cloneArray = array.slice();
   for (let i = 0; i < randomLength; i++) {
-    let randomInteger = getRandomNumber(0, cloneArray.length-1);
+    const randomInteger = getRandomNumber(0, cloneArray.length-1);
     randomArray[i] = cloneArray[randomInteger];
     cloneArray.splice(randomInteger, 1);
-  };
+  }
   return randomArray;
 };
+
 const createAnnouncement = (numberImage) => {
   const LAT_RANDOM = getRandomNumberComma(LAT_MIN, LAT_MAX, COMMA_LAT_LNG);
   const LNG_RANDOM = getRandomNumberComma(LNG_MIN, LNG_MAX, COMMA_LAT_LNG);
-  const avatarValue = 'img/avatars/user' + numberImage + '.png';
+  numberImage===10 ? numberImage : numberImage = `0${numberImage}`;
+  const avatarValue = `img/avatars/user${numberImage}.png`;
   return {
     author: {
       avatar: avatarValue,
     },
     offer: {
-      title: getRandomArrayElement(TITLE_OFFER),
-      address: LAT_RANDOM + ', ' + LNG_RANDOM,
+      title: getRandomArrayElement(OFFER_TITLES),
+      address: `${LAT_RANDOM}, ${LNG_RANDOM}`,
       price: getRandomNumber(1, 9999999),
-      type: getRandomArrayElement(TYPE_BUILDING),
+      type: getRandomArrayElement(BUILDING_TYPES),
       rooms: getRandomNumber(1, 999),
       guests: getRandomNumber(1, 9999),
-      checkin: getRandomArrayElement(CHECKIN_AND_CHECKOUT),
-      checkout: getRandomArrayElement(CHECKIN_AND_CHECKOUT),
-      features: getArrayElements(FEATURES_OFFER),
-      description: getRandomArrayElement(DESCRIPTION_OFFER),
-      photos: getArrayElements(PHOTOS_OFFER),
+      checkin: getRandomArrayElement(CHECKIN_AND_CHECKOUT_TIMES),
+      checkout: getRandomArrayElement(CHECKIN_AND_CHECKOUT_TIMES),
+      features: getArrayElements(OFFER_FEATURES),
+      description: getRandomArrayElement(OFFER_DESCRIPTIONS),
+      photos: getArrayElements(OFFER_PHOTOS),
     },
     location: {
       lat: LAT_RANDOM,
@@ -106,9 +107,12 @@ const createAnnouncement = (numberImage) => {
     },
   };
 };
-const newAnnouncement = [];
-for (let i = 1; i <= 10; i++) {
-  newAnnouncement[i-1] = createAnnouncement(i);
+const createAnnouncements = (numberAnnouncements) => {
+  const newAnnouncements = [];
+  for (let i = 0; i < numberAnnouncements; i++) {
+    newAnnouncements[i] = createAnnouncement(i+1);
+  }
+  return newAnnouncements;
 };
 
-console.log(newAnnouncement);
+createAnnouncements(NUMBER_ANNOUNCEMENT);
